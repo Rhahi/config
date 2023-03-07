1809de1940249417919e2998b9074f450135285a
Rhahi <git@rhahi.com>
2023-03-07 17:18:00 +0100

    create main

---
+++ README.md
@@ -0,0 +1,18 @@
+# using this configuration
+
+## copy the repo
+> git clone https://github.com/Rhahi/config.git
+
+if you're rhahi, use SSH instead
+
+> git@github.com:Rhahi/config.git
+
+## merge base into main
+
+> git merge base
+
+## merge other scripts as necessary
+
+this index will hopefully be maintained as new branches are added
+
+> git merge microbit
---
+++ config.fish
@@ -0,0 +1,16 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    for file in ~/.config/fish/userconfig/*
+        source $file
+    end
+
+    if test -e ~/notes/motd
+        function fish_greeting
+            cat ~/notes/motd
+        end
+    else
+        set -U fish_greeting
+    end
+end
+
+set -g fish_prompt_pwd_dir_length 6
