31733c3449e2c4aa9030e1643460d64975d56efa
Rhahi <git@rhahi.com>
2023-03-07 17:31:16 +0100

    add firewalld commands

---
+++ userconfig/firewalld.fish
@@ -0,0 +1,7 @@
+
+# firewalls
+abbr --add -- fwr "sudo firewall-cmd --reload"
+abbr --add -- fw "sudo firewall-cmd"
+abbr --add -- fwp "sudo firewall-cmd --permanent"
+abbr --add -- fwiz "fw --info-zone"
+abbr --add -- fwip "fw --info-policy"
