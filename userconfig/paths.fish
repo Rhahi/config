76617c7452f826677d3f8492d2ed6751e8837ee5
Rhahi <git@rhaih.com>
2023-03-11 13:54:15 +0000

    update: paths

--- userconfig/paths.fish
+++ userconfig/paths.fish
@@ -14,6 +14,9 @@ addpath "$HOME/.juliaup/bin"
 addpath "/opt/bin"
 addpath "$HOME/.local/tarball/bin"
 addpath "$HOME/.local/tarball/gcc-arm-none-eabi/bin"
+addpath "$HOME/go/bin"
+addpath "/usr/local/go/bin"
+addpath "/usr/sbin"
 
 abbr --add GCC10 CC=/usr/bin/gcc-10 CXX=/usr/bin/g++-10
 abbr --add GCC12 CC=/usr/bin/gcc-12 CXX=/usr/bin/g++-12
