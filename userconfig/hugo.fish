0d74aa51764c5667bb3490277cf0713da1ecf37a
Rhahi <git@rhahi.com>
2023-03-11 16:33:03 +0100

    fix: make serve echo command back

--- userconfig/hugo.fish
+++ userconfig/hugo.fish
@@ -5,5 +5,6 @@ addpath "$HOME/go/bin"
 
 # serve
 function hugo_dev
+  echo "hugo server -D --bind $argv[1] --baseURL --http://$argv[1] $argv[2..]"
   hugo server -D --bind $argv[1] --baseURL http://$argv[1] $argv[2..]
 end
