d135f6c98f79bc10683455e270cf8afda7a4c223
Rhahi <git@rhahi.com>
2023-03-22 20:52:45 +0100

    feat: add install shortcut

--- userconfig/hugo.fish
+++ userconfig/hugo.fish
@@ -3,6 +3,12 @@
 # register path
 addpath "$HOME/go/bin"
 
+function hugo_install
+  # https://gohugo.io/installation/linux/
+  go install -tags extended github.com/gohugoio/hugo@latest
+  hugo version
+end
+
 # serve
 function hugo_dev
   echo "hugo server -D --bind $argv[1] --baseURL --http://$argv[1] $argv[2..]"
