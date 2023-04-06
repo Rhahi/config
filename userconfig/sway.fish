7acd62e698ebc231e49ddcf6386ad1275b11a737
Rhahi <git@rhahi.com>
2023-04-06 21:44:23 +0200

    feat: add ozone for agent

--- userconfig/sway.fish
+++ userconfig/sway.fish
@@ -1,2 +1,3 @@
 alias code="code --enable-features=UseOzonePlatform --ozone-platform=wayland"
-
+alias agent="agent --ozone-platform=wayland"
+abbr --add --position anywhere oz -- "--ozone-platform=wayland"
