26634a5269838115919336d2d575d796208747b4
Rhahi <git@rhahi.com>
2023-07-27 22:06:20 +0200

    refactor: update path config

--- userconfig/paths.fish
+++ userconfig/paths.fish
@@ -7,13 +7,19 @@ function addpath
     end
 end
 
+# .local paths
 addpath "$HOME/.local/bin"
-addpath "$HOME/.local/image"
-addpath "$HOME/.cargo/bin"
-addpath "$HOME/.juliaup/bin"
-addpath "/opt/bin"
 addpath "$HOME/.local/tarball/bin"
 addpath "$HOME/.local/tarball/gcc-arm-none-eabi/bin"
+
+# .install paths (custom)
+addpath "$HOME/.install/image"
+
+# program paths
+addpath "$HOME/.cargo/bin"
+addpath "$HOME/.juliaup/bin"
+addpath "$HOME/go/bin"
+addpath "/opt/bin"
 
 abbr --add GCC10 CC=/usr/bin/gcc-10 CXX=/usr/bin/g++-10
 abbr --add GCC12 CC=/usr/bin/gcc-12 CXX=/usr/bin/g++-12
