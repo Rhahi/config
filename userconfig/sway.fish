26ea034b830b5340ba4914ca959a58fdeaeffe9c
Rhahi <git@rhahi.com>
2024-10-03 11:01:50 +0200

    shorten and fix sway commands
    resolve conflict

--- userconfig/sway.fish
+++ userconfig/sway.fish
@@ -1,8 +1,7 @@
 # compatibility abbreviation
 abbr --add --position anywhere ozone -- "--ozone-platform=wayland"
 
-
-abbr --add -- sway-display "swaymsg -t outputs"
+abbr --add -- sway-display "swaymsg -t get_outputs"
 abbr --add -- sway-reload "swaymsg reload"
-abbr --add -- sway-lidclose "swaymsg output eDP-1 disable"
-abbr --add -- sway-lidopen "swaymsg output eDP-1 enable"
+abbr --add -- lidclose "swaymsg output eDP-1 disable"
+abbr --add -- lidopen "swaymsg output eDP-1 enable"
